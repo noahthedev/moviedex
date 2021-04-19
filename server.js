b8721578e256c986f367a7debf0bd7f1991d739f
Noah Roberts <noahmroberts@gmail.com>
2021-04-19 09:05:17 -0500

    final deployment updates

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const movies = require('./movies-data-small.json')
 
 const app = express()
 
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 app.use(morgan('dev'))
 app.use(cors())
 app.use(helmet())
@@ -45,4 +46,16 @@ app.get('/movie', function handleGetMovie(req, res) {
   res.json(response)
 })
 
-app.listen(process.env.PORT)
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT)
